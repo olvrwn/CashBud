f88213811d8b801997aae44b2a6721c524f4c98e
olvrwn <oliver.dieke@icloud.com>
2023-09-18 09:52:36 +0200

    Implemented Mocks manually
    because Sweep is not able to generate meaningful and working ones

--- CashBudTests/Mocks/Services/LocalPersistenceServiceMock.swift
+++ CashBudTests/Mocks/Services/LocalPersistenceServiceMock.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+@testable import CashBud
+
+final class LocalPersistenceServiceMock: LocalPersistenceServiceProtocol {
+    
+    var writeToDocumentsDirectoryResult: Result<Void, Error>?
+    var readFromDocumentsDirectoryResult: Result<[Transaction], Error>?
+    
+    func writeToDocumentsDirectory<T>(into filename: String, data: T) throws where T: Codable {
+        
+        switch self.writeToDocumentsDirectoryResult {
+        case .success:
+            return ()
+            
+        case .failure(let failure):
+            throw failure
+            
+        case .none:
+            fatalError("writeToDocumentsDirectoryResult not set")
+        }
+    }
+    
+    func readFromDocumentsDirectory<T>(from filename: String) throws -> T where T: Codable {
+        
+        switch self.readFromDocumentsDirectoryResult {
+        case .success(let success):
+            return success as! T
+            
+        case .failure(let failure):
+            throw failure
+            
+        case .none:
+            fatalError("readFromDocumentsDirectoryResult not set")
+        }
+    }
+}
