14f7bb19e20eeaf5f3b5acf6318bee656f7abc0f
Oliver Dieke <82081459+olvrwn@users.noreply.github.com>
2023-09-24 10:36:59 +0200

    Added mocks
    Added mocks to this branch because of issues with Sweep switching to another branch

--- CashBudTests/Mocks/Managers/TransactionsManagerMock.swift
+++ CashBudTests/Mocks/Managers/TransactionsManagerMock.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+@testable import CashBud
+
+final class TransactionsManagerMock: TransactionsManagerProtocol {
+    
+    var readFromDocumentsDirectoryResult: Result<[Transaction], Error>?
+    var writeToDocumentsDirectoryResult: Result<Void, Error>?
+    var deleteResult: Result<Void, Error>?
+    var calculateMonthlyCostsResponse: Double?
+    var sortTransactionResponse: [CashBud.Transaction]?
+    var getIconStringResponse: String?
+    
+    func readFromDocumentsDirectory<T>(from filename: String) throws -> T where T : Decodable, T : Encodable {
+        
+        switch self.readFromDocumentsDirectoryResult {
+        case .success(let success):
+            return success as! T
+            
+        case .failure(let failure):
+            throw failure
+            
+        case .none:
+            fatalError("readFromDocumentsDirectoryResult not set")
+        }
+    }
+    
+    func writeToDocumentsDirectory(into filename: String, data: some Decodable & Encodable) throws {
+        
+        switch self.writeToDocumentsDirectoryResult {
+        case .success:
+            return
+            
+        case .failure(let failure):
+            throw failure
+            
+        case .none:
+            fatalError("writeToDocumentsDirectoryResult not set")
+        }
+    }
+    
+    func delete(id: UUID) throws {
+        
+        switch self.deleteResult {
+        case .success:
+            return
+            
+        case .failure(let failure):
+            throw failure
+            
+        case .none:
+            fatalError("deleteResult not set")
+        }
+    }
+    
+    func calculateMonthlyCosts(recurrence: CashBud.TransactionRecurrence, costs: Double) -> Double {
+        
+        guard let response = self.calculateMonthlyCostsResponse else {
+            fatalError("calculateMonthlyCostsResponse not set")
+        }
+        return response
+    }
+    
+    func sortTransactions(_ transactions: [CashBud.Transaction]) -> [CashBud.Transaction] {
+    
+        guard let response = self.sortTransactionResponse else {
+            fatalError("sortTransactionsResponse not set")
+        }
+        return response
+    }
+    
+    func getIconString(transactionCategory: CashBud.TransactionCategory) -> String {
+        
+        guard let response = self.getIconStringResponse else {
+            fatalError("getIconString not set")
+        }
+        return response
+    }
+}
